be713b917f9e7311e3e91e1d90122d5866d328dd
zdiboll <zdiboll@proforma.com>
2026-02-13 15:59:12 -0500

    Step 4 complete

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,5 +11,5 @@ selectedHero?: Hero;
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
-    heroes = HEROES;
+    heroes: Hero[] = [];
   };
