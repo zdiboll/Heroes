e151a3d22c2f42f92672816f9429ffa05bc11fe0
zdiboll <zdiboll@proforma.com>
2026-02-13 15:12:58 -0500

    Step 3 complete

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
 }
+    heroes = HEROES;
+  };
