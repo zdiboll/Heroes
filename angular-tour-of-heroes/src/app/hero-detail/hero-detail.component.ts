5749bf762bf991601974323c33f3b98b291d36cf
zdiboll <zdiboll@proforma.com>
2026-02-14 13:44:47 -0500

    Step 4 complete

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
--- angular-tour-of-heroes/src/app/heroes.service.ts
+++ angular-tour-of-heroes/src/app/heroes.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
-import {HEROES} from './mock-heroes';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class HeroesService {
-getHeroes(): Hero[] {
-  return HEROES;
-}
-  constructor() { }
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
 }
